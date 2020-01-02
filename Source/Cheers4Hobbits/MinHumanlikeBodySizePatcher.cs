b702b8b8f5497cef205cbb14c36a8538fedf630e
lbmaian <maian330@gmail.com>
2020-01-02 06:18:06 -0800

    Update readme, misc code update (no functional change)

--- Source/Cheers4Hobbits/MinHumanlikeBodySizePatcher.cs
+++ Source/Cheers4Hobbits/MinHumanlikeBodySizePatcher.cs
@@ -8,16 +8,16 @@ namespace Cheers4Hobbits
 {
 	class MinHumanlikeBodySizePatcher
 	{
-		static readonly MethodInfo pawnBodySizeGetter = typeof(Pawn).GetProperty(nameof(Pawn.BodySize)).GetGetMethod();
-		static readonly MethodInfo minHumanLikeBodySizeMethod =
+		static readonly MethodInfo methodof_Pawn_get_BodySize = typeof(Pawn).GetProperty(nameof(Pawn.BodySize)).GetGetMethod();
+		static readonly MethodInfo methodof_MinHumanlikeBodySize =
 			typeof(MinHumanlikeBodySizePatcher).GetMethod(nameof(MinHumanlikeBodySize), AccessTools.all);
 
 		public static IEnumerable<CodeInstruction> MinHumanlikeBodySizeTranspiler(IEnumerable<CodeInstruction> instructions)
 		{
 			foreach (var instruction in instructions)
 			{
-				if (instruction.opcode == OpCodes.Callvirt && instruction.operand == pawnBodySizeGetter)
-					yield return new CodeInstruction(OpCodes.Call, minHumanLikeBodySizeMethod);
+				if (instruction.opcode == OpCodes.Callvirt && instruction.operand == methodof_Pawn_get_BodySize)
+					yield return new CodeInstruction(OpCodes.Call, methodof_MinHumanlikeBodySize);
 				else
 					yield return instruction;
 			}
